192770e4f828b6ce00c1f5d8aedd1a45f38cd090
Max Halldén <max.hallden@gmail.com>
2015-09-05 23:07:56 +0200

    Added Tiled loader. Only works with spritesheets.
    Either add support for single images or just use spritesheets.
    Should probably be using spritesheets anyway so...

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 -- Configuration
 function love.conf(t)
     t.title = "Not Zombies"
-    t.window.width = 480
+    t.window.width = 1200
     t.window.height = 800
 
     t.console = true
--- main.lua
+++ main.lua
@@ -1,128 +1,38 @@
-debug = true
+-- small demo for TiledMap loader
+love.filesystem.load("tiledmap.lua")()
 
-score = 0
+gKeyPressed = {}
+gCamX,gCamY = 100,100
 
--- Timers
-canShoot = true
-canShootTimerMax = 0.2
-canShootTimer = canShootTimerMax
-
-createEnemyTimerMax = 0.4
-createEnemyTimer = createEnemyTimerMax
-
--- Image storage
-bulletImg = nil
-enemyImg = nil
-
--- Entity storage
-bullets = {}
-enemies = {}
-
-function love.load(arg)
-    player = { x = 200, y = 710, speed = 150, img = nil }
-    player.img = love.graphics.newImage('assets/plane.png')
-    bulletImg = love.graphics.newImage('assets/bullet.png')
-    enemyImg = love.graphics.newImage('assets/enemy.png')
+function love.load()
+    TiledMap_Load("map/map01.tmx")
 end
 
-function love.update(dt)
-    if love.keyboard.isDown('escape') then
-        love.event.push('quit')
-    end
-    
-    -- PLAYER CONTROL --
-    if love.keyboard.isDown('left', 'a') then
-        player.x = player.x - (player.speed*dt)
-    elseif love.keyboard.isDown('right', 'd') then
-        player.x = player.x + (player.speed*dt)
-    end
+function love.keyreleased( key )
+    gKeyPressed[key] = nil
+end
 
-    -- Bound check
-    if player.x <  0 then
-        player.x = 0
-    elseif player.x > (love.graphics.getWidth() - player.img:getWidth()) then
-        player.x = love.graphics.getWidth() - player.img:getWidth()
-    end
-
-    canShootTimer = canShootTimer - (1 * dt)
-    if canShootTimer < 0 then
-        canShoot = true
-    end
-
-    if canShoot and love.keyboard.isDown(' ', 'ctrl') then
-        newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
-        table.insert(bullets, newBullet)
-        canShoot = false
-        canShootTimer = canShootTimerMax
-    end
-
-    -- ENTITY CONTROL --
-    for i, bullet in ipairs(bullets) do
-        bullet.y = bullet.y - (250 * dt)
-        
-        if bullet.y < 0 then
-            table.remove(bullets, i)
-        end
-    end
-
-    -- ENEMY CONTROL --
-    createEnemyTimer = createEnemyTimer - (1 * dt)
-    if createEnemyTimer < 0 then
-        createEnemyTimer = createEnemyTimerMax
-
-        randomNumber = math.random(10, love.graphics.getWidth() - 10)
-        newEnemy = { x = randomNumber, y = -10, img = enemyImg }
-        table.insert(enemies, newEnemy)
-    end
-
-    for i, enemy in ipairs(enemies) do
-        enemy.y = enemy.y + (200 * dt)
-
-        if enemy.y > 850 then
-            table.remove(enemies, i)
-        end
-    end
-
-    -- COLLISION
-    for i, enemy in ipairs(enemies) do
-        for j, bullet in ipairs(bullets) do
-            if (CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(),
-                bullet.x, bullet.y, bullet.img:getWidth(), bullet.img:getHeight())) then
-                table.remove(bullets, j);
-                table.remove(enemies, i);
-                score = score + 1
-            end
-        end
-        if (CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(),
-            player.x, player.y, player.img:getWidth(), player.img:getHeight())) then
-            table.remove(enemies, i);
-            score = score - 1
-        end
+function love.keypressed( key, unicode ) 
+    gKeyPressed[key] = true 
+    if (key == "escape") then os.exit(0) end
+    if (key == " ") then -- space = next mal
+        gMapNum = (gMapNum or 1) + 1
+        if (gMapNum > 10) then gMapNum = 1 end
+        TiledMap_Load(string.format("map/map%02d.tmx",gMapNum))
+        gCamX,gCamY = 100,100
     end
 end
 
-function love.draw(dt)
-    love.graphics.print(score, love.graphics:getWidth()-50, 10)
-
-    love.graphics.draw(player.img, player.x, player.y)
-
-    for i, bullet in ipairs(bullets) do
-        love.graphics.draw(bullet.img, bullet.x, bullet.y)
-    end
-
-    for i, enemy in ipairs(enemies) do
-        love.graphics.draw(enemy.img, enemy.x + enemy.img:getWidth(), 
-        enemy.y + enemy.img:getHeight(), math.pi)
-    end
+function love.update( dt )
+    local s = 500*dt
+    if (gKeyPressed.up) then gCamY = gCamY - s end
+    if (gKeyPressed.down) then gCamY = gCamY + s end
+    if (gKeyPressed.left) then gCamX = gCamX - s end
+    if (gKeyPressed.right) then gCamX = gCamX + s end
 end
 
--- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
--- Returns true if two boxes overlap, false if they don't
--- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
--- x2,y2,w2 & h2 are the same, but for the second box
-function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
-  return x1 < x2+w2 and
-         x2 < x1+w1 and
-         y1 < y2+h2 and
-         y2 < y1+h1
+function love.draw()
+    love.graphics.print('arrow-keys=scroll, space=next map', 50, 50)
+    love.graphics.setBackgroundColor(0x80,0x80,0x80)
+    TiledMap_DrawNearCam(gCamX,gCamY)
 end
